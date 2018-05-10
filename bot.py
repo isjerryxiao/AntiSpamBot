b3ee7901f38e4e36c0a32aa726fea465e68d7035
Jerry <Jerry981028@gmail.com>
2018-05-10 18:21:50 +0800

    Bug:   When admins invite bots, the bot will be banned but there are no buttons to unban. Fix:   Now the bot won't ban a bot if it is invited by an admin.

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from datetime import datetime, timedelta
 from time import time
-from telegram.error import TelegramError
+from telegram.error import TelegramError, BadRequest
 
 logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -43,28 +43,52 @@ def display_username(user, atuser=True, shorten=False):
 
 
 def ban_user(bot, chat_id, user, invite_user):
+    bot_banned = False
     try:
-        if bot.restrict_chat_member(chat_id=chat_id, user_id=user.id, until_date=datetime.utcnow()+timedelta(days=367)) and \
-                    bot.restrict_chat_member(chat_id=chat_id, user_id=invite_user.id, until_date=datetime.utcnow()+timedelta(days=367)):
-            button = InlineKeyboardButton(text="解除封禁", callback_data="unban {0} {1}".format(user.id, invite_user.id))
-            bot.send_message(chat_id=chat_id,
-                    text="发现新加入的bot: {0} ，以及拉入bot的用户: {1} ，已经将其全部封禁。"
-                            "如需解封请管理员点击下面的按钮。".format(display_username(user), display_username(invite_user)),
-                            reply_markup=InlineKeyboardMarkup([[button]]))
-            logger.info("Banned {0} and {1} in the group {2}".format(user.id, invite_user.id, chat_id))
+        if bot.restrict_chat_member(chat_id=chat_id, user_id=user.id, until_date=datetime.utcnow()+timedelta(days=367)):
+            bot_banned = True
+            if bot.restrict_chat_member(chat_id=chat_id, user_id=invite_user.id, until_date=datetime.utcnow()+timedelta(days=367)):
+                button = InlineKeyboardButton(text="解除封禁", callback_data="unban {0} {1}".format(user.id, invite_user.id))
+                bot.send_message(chat_id=chat_id,
+                        text="发现新加入的bot: {0} ，以及拉入bot的用户: {1} ，已经将其全部封禁。"
+                                "如需解封请管理员点击下面的按钮。".format(display_username(user), display_username(invite_user)),
+                                reply_markup=InlineKeyboardMarkup([[button]]))
+                logger.info("Banned {0} and {1} in the group {2}".format(user.id, invite_user.id, chat_id))
+            else:
+                raise TelegramError
         else:
             raise TelegramError
-    except TelegramError:
-        bot.send_message(chat_id=chat_id,
-                text="发现新加入的bot: {0} ，但机器人不是管理员导致无法实施有效行动。"
-                        "请将机器人设为管理员并打开封禁权限。".format(display_username(user)))
-        logger.info("Cannot ban {0} in the group {1}".format(user.id, chat_id))
+    except (TelegramError, BadRequest):
+        if bot_banned:
+            admin_ids = list()
+            for chat_member in bot.get_chat_administrators(chat_id):
+                admin_ids.append(chat_member.user.id)
+            if invite_user.id in admin_ids:
+                unban_user(bot, [user.id], callback_mode=False, non_callback_chat_id=chat_id)
+                logger.info("Admin {1} invited bot {0} in the group {2}".format(user.id, invite_user.id, chat_id))
+            else:
+                button = InlineKeyboardButton(text="解除封禁", callback_data="unban {0}".format(user.id))
+                bot.send_message(chat_id=chat_id,
+                        text="发现新加入的bot: {0} ，以及拉入bot的用户: {1} 。\n"
+                                "由于未知原因拉入bot的用户无法封禁，已经将bot封禁。"
+                                "如需解封请管理员点击下面的按钮。".format(display_username(user), display_username(invite_user)),
+                                reply_markup=InlineKeyboardMarkup([[button]]))
+                logger.info("Banned {0} but not {1} in the group {2}".format(user.id, invite_user.id, chat_id))
+        else:
+            bot.send_message(chat_id=chat_id,
+                    text="发现新加入的bot: {0} ，但机器人不是管理员导致无法实施有效行动。"
+                            "请将机器人设为管理员并打开封禁权限。".format(display_username(user)))
+            logger.info("Cannot ban {0} and {1} in the group {2}".format(user.id, invite_user.id, chat_id))
 
 
-def unban_user(bot, unban_ids, update):
-    chat_id = update.callback_query.message.chat.id
-    message = update.callback_query.message
-    user = update.callback_query.from_user
+def unban_user(bot, unban_ids, update=None, callback_mode=True, non_callback_chat_id=None):
+    if not callback_mode:
+        assert update == None
+        chat_id = non_callback_chat_id
+    else:
+        chat_id = update.callback_query.message.chat.id
+        message = update.callback_query.message
+        user = update.callback_query.from_user
     for unban_id in unban_ids:
         try:
             if bot.restrict_chat_member(chat_id=chat_id, user_id=unban_id,
@@ -76,6 +100,8 @@ def unban_user(bot, unban_ids, update):
         except TelegramError:
             logger.info("Cannot unban {0} in the group {1}".format(unban_id, chat_id))
             return
+    if not callback_mode:
+        return
     try:
         bot.edit_message_text(chat_id=chat_id, message_id=message.message_id, text=message.text + "\n\n解封成功。操作人 {0}".format(display_username(user, atuser=False)), reply_markup=None)
     except:
@@ -114,10 +140,11 @@ def at_admins(bot, update):
                 update.message.delete()
             except TelegramError:
                 logger.info("Unable to delete at_admin spam message {0} from {1}".format(update.message.message_id, update.message.from_user.id))
+            else:
+                logger.info("Deleted at_admin spam messages {0} and {1} from {2}".format(update.message.message_id, notice.message_id, update.message.from_user.id))
             notice.delete()
         job_queue.run_once(delete_notice, 5)
         job_queue.start()
-        logger.info("Deleted at_admin spam messages {0} and {1} from {2}".format(update.message.message_id, notice.message_id, update.message.from_user.id))
         return
     admins = list()
     for chat_member in bot.get_chat_administrators(chat_id):
